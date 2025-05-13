89b3fcb8e87ab32a879f83e7d7a54378c7b2e411
Suhas Deshpande <suhas2u@gmail.com>
2025-05-12 19:45:08 -0700

    updates cpk to next

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,9 @@ import { CopilotPopup } from "@copilotkit/react-ui";
 import "@copilotkit/react-ui/styles.css";
 
 export default function Home() {
-  const COPILOT_CLOUD_PUBLIC_API_KEY = process.env.NEXT_PUBLIC_COPILOT_CLOUD_PUBLIC_API_KEY;
-
   return (
     <>
-      <CopilotKit publicApiKey={COPILOT_CLOUD_PUBLIC_API_KEY}>
+      <CopilotKit runtimeUrl="/api/copilotkit">
         <TasksProvider>
           <TasksList />
         </TasksProvider>

63d22fbdb2dfa65729a82069836ff0a3c06aedaa
Steve Cohen <schlomoc@gmail.com>
2025-06-11 10:08:17 -0700

    feat: integrate CopilotKit for AI-powered task management with sidebar interface

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,18 @@
 
 import { TasksList } from "@/components/TasksList";
 import { TasksProvider } from "@/lib/hooks/use-tasks";
+import { CopilotKit } from "@copilotkit/react-core"; 
+import { CopilotSidebar } from "@copilotkit/react-ui"; 
+import "@copilotkit/react-ui/styles.css"; 
+
 
 export default function Home() {
   return (
-    <TasksProvider>
-      <TasksList />
-    </TasksProvider>
+<CopilotKit publicApiKey={process.env.NEXT_PUBLIC_COPILOT_PUBLIC_API_KEY!}>
+      <TasksProvider>
+        <TasksList />
+      </TasksProvider>
+      <CopilotSidebar />
+    </CopilotKit>
   );
 }
